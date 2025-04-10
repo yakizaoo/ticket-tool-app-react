a89794ba559183be3f05507f697ac4f01ceeb6cd
artem <6superiority@gmail.com>
2025-04-10 23:22:13 +0300

    Delete server directory

--- server/src/routes/api.js
+++
@@ -1,8 +0,0 @@
-// Users routes
-router.get('/users', auth, UserController.getAll);
-router.get('/users/:id', auth, UserController.getById);
-router.post('/users', auth, UserController.create);
-router.put('/users/:id', auth, UserController.updateById);
-router.delete('/users/:id', auth, UserController.delete);
-router.post('/users/:id/deactivate', auth, UserController.deactivateUser);
-router.post('/users/:id/activate', auth, UserController.activateUser); 
